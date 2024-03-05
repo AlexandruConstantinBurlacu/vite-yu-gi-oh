560c10cef0b54d5addef2dafe2bf3c1d1a3d84a9
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-03-05 15:16:52 +0100

    inserimento componente lista carte

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,18 @@
 
   import axios from 'axios';
 
+  import { store } from './store';
+
+  import CardsList from './components/CardsList.vue';
+
+
   export default {
     data() {
       return {
+
+        cards: [],
+
+        store,
         
       }
     },
@@ -12,7 +21,8 @@
     created() {
 
       axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then(res => {
-
+        console.log(res.data.data),
+        this.store.cards = res.data.data
       })
       
     },
@@ -22,7 +32,7 @@
     },
 
     components: {
-
+      CardsList,
     },
 
     methods: {
@@ -34,7 +44,23 @@
 </script>
 
 <template>
-  prova
+
+  <div class="container">
+
+    <ul>
+    <li v-for="currentCard in cards">
+    
+      {{ currentCard.name }}
+
+      
+    
+    </li>
+  </ul>
+
+  </div>
+
+  
+
 </template>
 
 <style lang="scss">
---
+++ src/components/CardsList.vue
@@ -0,0 +1,26 @@
+<script>
+
+    export default {
+        name: 'CardsList',
+
+        data() {
+            return {
+                
+            }
+        },
+
+    }
+
+</script>
+
+
+<template>
+        
+    <h1>Carte Yu Gi Oh</h1>
+
+</template>
+
+
+<style lang="scss">
+
+</style>
