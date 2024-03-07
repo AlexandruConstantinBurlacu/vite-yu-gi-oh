f98dfbee797d373d6a1b4e304d1e16dd84cb5221
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-03-07 10:27:01 +0100

    inserimento cardslist in pagina

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script>
 
   import axios from 'axios';
-
   import { store } from './store';
-
   import CardsList from './components/CardsList.vue';
 
 
@@ -11,31 +9,21 @@
     data() {
       return {
 
-        cards: [],
-
         store,
         
       }
-    },
-
-    created() {
-
-      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then(res => {
-        console.log(res.data.data),
-        this.store.cards = res.data.data
-      })
-      
-    },
-
-    mounted() {
-      
     },
 
     components: {
       CardsList,
     },
 
-    methods: {
+    created() {
+
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then(res => {
+        console.log(res.data.data);
+        this.store.cards = res.data.data
+      })
       
     },
 
@@ -45,21 +33,7 @@
 
 <template>
 
-  <div class="container">
-
-    <ul>
-    <li v-for="currentCard in cards">
-    
-      {{ currentCard.name }}
-
-      
-    
-    </li>
-  </ul>
-
-  </div>
-
-  
+  <CardsList></CardsList>
 
 </template>
 
